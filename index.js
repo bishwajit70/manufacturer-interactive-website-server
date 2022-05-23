e2794efe10255d41b5b84eba112a888fbcac329d
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-23 13:10:17 +0600

    order api is working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000
@@ -32,6 +33,7 @@ async function run() {
 
     const productCollection = client.db('manufacturer_portal').collection('products')
     const orderCollection = client.db('manufacturer_portal').collection('orders')
+    const userCollection = client.db('manufacturer_portal').collection('users')
 
     app.get('/product', async (req, res) => {
       const query = {};
@@ -47,13 +49,48 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/order', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const orders = await orderCollection.find(query).toArray()
+      res.send(orders);
+    })
+
+
+
     app.post('/order', async (req, res) => {
       const order = req.body;
       console.log('Adding New inventory Item', order);
       const result = await orderCollection.insertOne(order);
       console.log(result);
       res.send(result)
-  })
+    })
+
+    // create user or update user 
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d',
+      })
+      // res.send(result);
+      res.send({ result, token });
+
+    })
+
+
+    // app.get('/user', async (req, res) => {
+    //   const users = await userCollection.find().toArray()
+    //   res.send(users);
+    // })
+
+
 
     // app.get('/inventory/:id', async (req, res) => {
     //   const id = req.params.id;
