2035219a9d7ccaf84f26a988f87486a62ec3108d
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-23 10:15:49 +0600

    order api added

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     await client.connect();
 
     const productCollection = client.db('manufacturer_portal').collection('products')
+    const orderCollection = client.db('manufacturer_portal').collection('orders')
 
     app.get('/product', async (req, res) => {
       const query = {};
@@ -39,12 +40,20 @@ async function run() {
       res.send(products);
     })
 
-    app.get('/product/:id', async (req, res) => {
+    app.get('/purchase/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await productCollection.findOne(query);
       res.send(result);
     })
+
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      console.log('Adding New inventory Item', order);
+      const result = await orderCollection.insertOne(order);
+      console.log(result);
+      res.send(result)
+  })
 
     // app.get('/inventory/:id', async (req, res) => {
     //   const id = req.params.id;
